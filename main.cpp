6cb585e9c2b3c2a5599312baed134c9bf1e115c5
Viktor Leher <viktor.leher@gmail.com>
2021-04-03 20:40:12 +0200

    performance measurements

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
 
 #define DIMENSION 20
 typedef int Matrix[DIMENSION][DIMENSION];
 
-int two_by_two_determinant(const Matrix& matrix) {
+int two_by_two_determinant(const Matrix &matrix) {
     return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
 }
 
@@ -12,22 +13,22 @@ int minus_1_power_n(int n) {
     return n % 2 == 0 ? 1 : -1;
 }
 
-int determinantSerial(const Matrix& matrix, int dimension) {
+int determinantSerial(const Matrix &matrix, int dimension) {
 
-    if(dimension == 2) {
+    if (dimension == 2) {
         return two_by_two_determinant(matrix);
     }
 
     int determinant = 0;
 
-    for(int i=0; i<dimension; i++) {
+    for (int i = 0; i < dimension; i++) {
         Matrix smallMatrix;
 
-        for(int k=0; k<dimension; k++) {
-            for(int l=0; l<dimension; l++) {
-                if(k != 0 && l != i) {
-                    int newK = k-1;
-                    int newL = l > i ? l-1 : l;
+        for (int k = 0; k < dimension; k++) {
+            for (int l = 0; l < dimension; l++) {
+                if (k != 0 && l != i) {
+                    int newK = k - 1;
+                    int newL = l > i ? l - 1 : l;
                     smallMatrix[newK][newL] = matrix[k][l];
                 }
             }
@@ -40,51 +41,50 @@ int determinantSerial(const Matrix& matrix, int dimension) {
     return determinant;
 }
 
-int determinantParallel(const Matrix& matrix, int dimension, int threshold) {
+int determinantParallel(const Matrix &matrix, int dimension, int threshold) {
 
-    if(dimension == 2) {
+    if (dimension == 2) {
         return two_by_two_determinant(matrix);
     }
-
 
     int determinant = 0;
 
     if (dimension >= threshold) {
-        #pragma omp parallel
-        #pragma omp single nowait
+#pragma omp parallel
+#pragma omp single nowait
         {
-            for(int i=0; i<dimension; i++) {
-                #pragma omp task
+            for (int i = 0; i < dimension; i++) {
+#pragma omp task
                 {
                     Matrix smallMatrix;
 
-                    for(int k=0; k<dimension; k++) {
-                        for(int l=0; l<dimension; l++) {
-                            if(k != 0 && l != i) {
-                                int newK = k-1;
-                                int newL = l > i ? l-1 : l;
+                    for (int k = 0; k < dimension; k++) {
+                        for (int l = 0; l < dimension; l++) {
+                            if (k != 0 && l != i) {
+                                int newK = k - 1;
+                                int newL = l > i ? l - 1 : l;
                                 smallMatrix[newK][newL] = matrix[k][l];
                             }
                         }
                     }
 
                     int result = matrix[0][i] * minus_1_power_n(0 + i + 2) *
-                                                 determinantSerial(smallMatrix, dimension - 1);
+                                 determinantSerial(smallMatrix, dimension - 1);
 
-                    #pragma omp critical
+#pragma omp critical
                     determinant = determinant + result;
                 }
             }
         }
     } else {
-        for(int i=0; i<dimension; i++) {
+        for (int i = 0; i < dimension; i++) {
             Matrix smallMatrix;
 
-            for(int k=0; k<dimension; k++) {
-                for(int l=0; l<dimension; l++) {
-                    if(k != 0 && l != i) {
-                        int newK = k-1;
-                        int newL = l > i ? l-1 : l;
+            for (int k = 0; k < dimension; k++) {
+                for (int l = 0; l < dimension; l++) {
+                    if (k != 0 && l != i) {
+                        int newK = k - 1;
+                        int newL = l > i ? l - 1 : l;
                         smallMatrix[newK][newL] = matrix[k][l];
                     }
                 }
@@ -98,38 +98,68 @@ int determinantParallel(const Matrix& matrix, int dimension, int threshold) {
     return determinant;
 }
 
-void execSerial(const Matrix& matrix, int dimension) {
+void execSerial(const Matrix &matrix, int dimension) {
     auto t1 = std::chrono::high_resolution_clock::now();
     int determinant = determinantSerial(matrix, dimension);
     auto t2 = std::chrono::high_resolution_clock::now();
-    printf("determinant serial = %d and took %d ms\n", determinant, std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+    printf("determinant serial = %d and took %d ms\n", determinant,
+           std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 }
 
-void execParallel(const Matrix& matrix, int dimension, int threshold) {
+void execParallel(const Matrix &matrix, int dimension, int threshold) {
     auto t1 = std::chrono::high_resolution_clock::now();
     int determinant = determinantParallel(matrix, dimension, threshold);
     auto t2 = std::chrono::high_resolution_clock::now();
-    printf("determinant parallel = %d and took %d ms\n", determinant, std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+//    printf("determinant parallel = %d and took %d ms\n", determinant, std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+}
+
+double avgTimeMsSerial(int iterations, int dimension, const Matrix &matrix) {
+    double sumMs = 0;
+    for (int i = 0; i < iterations; i++) {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        int determinant = determinantSerial(matrix, dimension);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> duration = t2 - t1;
+        sumMs += duration.count();
+    }
+    return sumMs / iterations;
+}
+
+double avgTimeMsParallel(int iterations, int dimension, const Matrix &matrix, int threshold) {
+    double sumMs = 0;
+    for (int i = 0; i < iterations; i++) {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        int determinant = determinantParallel(matrix, dimension, threshold);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> duration = t2 - t1;
+        sumMs += duration.count();
+    }
+    return sumMs / iterations;
 }
 
 int main() {
-    int dimension = 11;
-    int thresholds[] = {3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int dimensions = 7;
+    int thresholds[] = {3, 4, 5, 6, 7, 8, 9, 10};
 
-    for(int threshold : thresholds) {
-        std::cout << "threshold " << threshold << std::endl;
+    Matrix matrix;
 
-        Matrix matrix;
-
-        for(int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
-                matrix[i][j] = i*dimension + j + 1;
-            }
+    for (int i = 0; i < dimensions; i++) {
+        for (int j = 0; j < dimensions; j++) {
+            matrix[i][j] = i * dimensions + j + 1;
         }
-
-        // execSerial(matrix, dimension);
-        execParallel(matrix, dimension, threshold);
     }
+
+//  comparing performance for [dimensions] dimensions with different thresholds
+    const int numberOfRunsPerDimension = 10;
+    std::ofstream csv;
+    csv.open ("performance.csv");
+    csv  << "threshold, execution time (ms), speedup" << std::endl;
+    auto speedSerial = avgTimeMsSerial(numberOfRunsPerDimension, dimensions, matrix);
+    for (auto threshold : thresholds) {
+        auto speedParallel = avgTimeMsParallel(numberOfRunsPerDimension, dimensions, matrix, threshold);
+        csv << threshold << "," << speedParallel << "," << speedSerial/speedParallel << std::endl;
+    }
+    csv.close();
 
     return 0;
 }
